a2066d4217f3c245ea0e7c51622d595f963b9c91
AdnaneElayachi <108431361+AdnaneElayachi@users.noreply.github.com>
2024-01-06 22:37:35 +0100

    2

---
+++ GCS_GAdministation/GCS_GAdministation/src/main/java/GCS/GCS_GAdministation/repository/AdministrationRepository.java
@@ -0,0 +1,11 @@
+package GCS.GCS_GAdministation.repository;
+
+import entity.Administration;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AdministrationRepository extends JpaRepository<Administration, Long> {
+
+
+}
