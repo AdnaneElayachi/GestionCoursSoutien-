b1e797029e9af552a7e7cc1aa61fd3e27a0c1525
AdnaneElayachi <108431361+AdnaneElayachi@users.noreply.github.com>
2024-01-08 00:38:02 +0100

    a

--- GCS/src/main/java/ex/GCS/GCS/entity/Professeur.java
+++ GCS/src/main/java/ex/GCS/GCS/entity/Professeur.java
@@ -17,17 +17,12 @@ public class Professeur {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
-    @Column(name = "compte")
-    private String compte;
 
     @Column(name = "nom")
     private String nom;
 
     @Column(name = "temps_enseignement")
     private Date tempsEnseignement;
-
-    @Column(name = "mot_de_passe")
-    private String motDePasse;
 
     @Column(name = "specialite")
     private String specialite;
